7fa053b9cafbbcea6c3ae1c6b395977eea411f53
Thrity Golzari <154635854+thritthegrit@users.noreply.github.com>
2025-01-05 21:13:06 -0800

    Completed all functions
    All functions work properly when called in main
    function as a loop.

--- chat.cpp
+++ chat.cpp
@@ -8,11 +8,13 @@ int userTurn = 1;
 
 void write() {
     string message;
+    cout<<"\n"<<endl;
     cout << "Type your message"<<endl;
-    cin >> message;
+    cin.ignore();
+    getline(cin, message);
     ofstream fout;
     fout.open("chatlog.txt", ios::app);
-    fout << "User " + to_string(userTurn) + ": " + message <<endl;
+    fout << "User " << userTurn << ": " << message <<endl;
     fout.close();
 }
 
@@ -20,10 +22,11 @@ void display(){
     string line;
     ifstream fin;
     fin.open("chatlog.txt");
-    while (fin >> line) {
+    cout<< "Chat History"<<endl;
+    while (getline(fin, line)) {
         cout<<line<<endl;
+        cout<<"\n"<<endl;
     }
-
     fin.close();
 }
 
@@ -31,10 +34,13 @@ int main() {
     ofstream("chatlog.txt");
     int choice;
     while (true) {
-        cout << "Select Choice By Number:";
+        cout << "Welcome to the chat! Currently set as User "<<userTurn<<endl;
+        cout << "Select Choice By Number:"<<endl;
         cout << "1. Write Message" <<endl;
         cout << "2. Display Messages" <<endl;
-        cout << "3. Exit" <<endl;
+        cout << "3. Switch User"<<endl;
+        cout << "4. Exit" <<endl;
+        cout << "\n"<<endl;
 
         cin >> choice;
         if (choice==1){
@@ -42,11 +48,21 @@ int main() {
         } else if (choice==2) {
             display();
         } else if (choice==3) {
+            if (userTurn==1) {
+                userTurn=2;
+                cout<<"Switched to User 2"<<endl;
+                cout<<"\n"<<endl;
+            } else {
+                userTurn=1;
+                cout<<"Switched to User 1"<<endl;
+                cout<<"\n"<<endl;
+            }
+        } else if (choice==4) {
             break;
         }
 
-        remove("chatlog.txt");
-        return 0;
-
+        
     }
+    remove("chatlog.txt");
+    return 0;
 }
